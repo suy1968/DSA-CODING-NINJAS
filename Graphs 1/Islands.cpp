b9dfc0c493ce0b055f338ec8b3a6466bf9ae60a6
Suyash Pratap Singh <72243026+suy1968@users.noreply.github.com>
2022-12-20 14:41:16 +0530

    Add files via upload

--- Graphs 1/Islands.cpp
+++ Graphs 1/Islands.cpp
@@ -1,95 +1,88 @@
-/*
-Islands
-An island is a small piece of land surrounded by water . A group of islands is said to be connected if we can reach from any given island to 
-any other island in the same group . Given V islands (numbered from 1 to V) and E connections or edges between islands. 
-Can you count the number of connected groups of islands.
-
-Input Format :
-The first line of input contains two integers, that denote the value of V and E.
-Each of the following E lines contains two integers, that denote that there exists an edge between vertex a and b. 
-
-Output Format :
-Print the count the number of connected groups of islands
-
+/* An island is a small piece of land surrounded by water . A group of islands is said to be connected if we can reach from any given island to any other island in the same group . Given N islands (numbered from 1 to N) and two lists of size M (u and v) denoting island u[i] is connected to island v[i] and vice versa . Can you count the number of connected groups of islands.
 Constraints :
-0 <= V <= 1000
-0 <= E <= (V * (V-1)) / 2
-0 <= a <= V - 1
-0 <= b <= V - 1
+1<=N<=100
+1<=M<=(N*(N-1))/2
+1<=u[i],v[i]<=N
+Input Format :
+Line 1 : Two integers N and M
+Line 2 : List u of size of M
+Line 3 : List v of size of M
+Output Return Format :
+The count the number of connected groups of islands
+Sample Input :
+2 1
+1
+2
+Sample Output :
+1  */
 
-Time Limit: 1 second
 
-Sample Input 1:
-5 8
-0 1
-0 4
-1 2
-2 0
-2 4
-3 0
-3 2
-4 3
-
-Sample Output 1:
-1 
-*/
 
 #include<iostream>
-#include<vector>
-#include<unordered_map>
 #include<queue>
-#define line '\n'
+#include<vector>
 using namespace std;
-
-void BFS(unordered_map<int, vector<int>>& graph, vector<bool>& visited, int start) {
-    queue<int> q;
-    q.push(start);
-    visited[start] = true;
-    
-    while(!q.empty()) {
-        int front = q.front();
-        q.pop();
-        
-        for(int i = 0; i < graph[front].size(); i++) {
-            int current = graph[front][i];
-            if(!visited[current]) {
-                q.push(current);
-                visited[current] = true;
-            }
-        }
-    }
+void visited_filler(int** arr, int n, int starting_vertex, bool* visited)
+{
+	queue<int> q;
+	q.push(starting_vertex);
+	visited[starting_vertex] = true;
+	while (!q.empty())
+	{
+		int current_element = q.front();
+		for (int i = 1; i < n; i++)
+		{
+			if (i == current_element)
+			{
+				continue;
+			}
+			if (visited[i])
+			{
+				continue;
+			}
+			if (arr[current_element][i] == 1)
+			{
+				q.push(i);
+				visited[i] = true;
+			}
+		}
+		q.pop();
+	}
 }
-
-int countComponents(unordered_map<int, vector<int>>& graph, int v) {
-    vector<bool> visited(v, false);
-    int count = 0;
-    for(int i = 0; i < v; i++) {
-        if(!visited[i]) {
-            count++;
-            BFS(graph, visited, i);
-        }
-    } 
+int solve(int n,int m,vector<int>u,vector<int>v)
+{
+	// Write your code here .
+    int** arr = new int*[n+1];
+	for (int i = 0; i < n+1; i++)
+	{
+		arr[i] = new int[n+1];
+		for (int j = 0; j < n+1; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
     
-    return count;
+    for (int i = 0; i < m; i++)
+	{
+		arr[u[i]][v[i]] = 1;
+		arr[v[i]][u[i]] = 1;
+	}
+    
+    bool* visited = new bool[n+1];
+	for (int i = 0; i < n+1; i++)
+	{
+		visited[i] = false;
+	}
+    
+    int count=0;
+
+	for (int i = 1; i < n+1; i++)
+	{
+		if (!visited[i])
+		{
+		    visited_filler(arr, n+1, i, visited);
+            count+=1;
+		}
+	}
+	return count;
 }
-
-int main() {
-    // Write your code here
-    int v, e;
-    cin >> v >> e;
-    
-    unordered_map<int, vector<int>> graph;
-    
-    
-    for(int i = 0; i < e; i++) {
-        int first, second;
-        cin >> first >> second;
-        graph[first].push_back(second);
-        graph[second].push_back(first);
-    }
-    
-    cout << countComponents(graph, v) << line;
-}
-
-// Time Complexity : O(V + E)
-// Auxillary Space : O(V)
